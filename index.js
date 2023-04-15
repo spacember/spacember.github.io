ab83f6dace3e2776cf989a7fdd915ab6ef41b51d
Aditya Ramdonee (RAPP) <aditya.ramdonee@rapp.com>
2023-04-15 17:41:26 +0400

    home page mobile responsive

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleNavbar() {
+  
+    const navbarItems = document.getElementById("navbarItems");
+    navbarItems.style.display === "block" ? navbarItems.style.display = "none" : navbarItems.style.display = "block";
+}
