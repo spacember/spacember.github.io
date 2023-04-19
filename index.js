e5fdde323b5530c32ca205fbbd045ad5ba9a8dc9
Aditya Ramdonee (RAPP) <aditya.ramdonee@rapp.com>
2023-04-19 14:08:17 +0400

    optimised navigation and folder structure

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
+"use strict";
+
 function toggleNavbar() {
-  
-    const navbarItems = document.getElementById("navbarItems");
-    navbarItems.style.display === "block" ? navbarItems.style.display = "none" : navbarItems.style.display = "block";
+  const navbarItems = document.getElementById("navbarItems");
+  navbarItems.style.display === "block"
+    ? (navbarItems.style.display = "none")
+    : (navbarItems.style.display = "block");
+}
+
+function viewProjects() {
+  window.open("projects.html");
+}
+
+let currentSection = "navbar";
+
+function goToNextSection() {
+  const buttonArrowUp = document.getElementById("buttonArrowUp");
+  const buttonArrowDown = document.getElementById("buttonArrowDown");
+
+  buttonArrowUp.style.display = "block";
+
+  if (currentSection === "navbar") {
+    currentSection = "portrait";
+    document.getElementById("portrait").scrollIntoView({ behavior: "smooth" });
+  } else if (currentSection === "portrait") {
+    currentSection = "skills";
+    document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
+    buttonArrowDown.style.display = "none";
+  }
+}
+
+function goTop() {
+  const buttonArrowUp = document.getElementById("buttonArrowUp");
+  const buttonArrowDown = document.getElementById("buttonArrowDown");
+
+  buttonArrowDown.style.display = "block";
+  buttonArrowDown.value = "navbar";
+
+  if (currentSection !== "navbar") {
+    currentSection = "navbar"
+    document.getElementById("navbar").scrollIntoView({ behavior: "smooth" });
+    buttonArrowUp.style.display = "none";
+  }
 }
