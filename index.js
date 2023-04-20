9573cc81a79cc317654ea4d887ea259ba17f485c
Aditya Ramdonee (RAPP) <aditya.ramdonee@rapp.com>
2023-04-20 13:28:01 +0400

    optimised html and css and added desktop responsiveness

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 function toggleNavbar() {
   const navbarItems = document.getElementById("navbarItems");
-  navbarItems.style.display === "block"
+  navbarItems.style.display === "flex"
     ? (navbarItems.style.display = "none")
-    : (navbarItems.style.display = "block");
+    : (navbarItems.style.display = "flex");
 }
 
 function viewProjects() {
