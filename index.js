cc8aecadc4c774351f6f223c073ad4a82837efcb
Aditya Ramdonee (RAPP) <aditya.ramdonee@rapp.com>
2023-04-20 22:16:45 +0400

    refined the overall design

--- index.js
+++ index.js
@@ -6,39 +6,3 @@ function toggleNavbar() {
     ? (navbarItems.style.display = "none")
     : (navbarItems.style.display = "flex");
 }
-
-function viewProjects() {
-  window.open("projects.html");
-}
-
-let currentSection = "navbar";
-
-function goToNextSection() {
-  const buttonArrowUp = document.getElementById("buttonArrowUp");
-  const buttonArrowDown = document.getElementById("buttonArrowDown");
-
-  buttonArrowUp.style.display = "block";
-
-  if (currentSection === "navbar") {
-    currentSection = "portrait";
-    document.getElementById("portrait").scrollIntoView({ behavior: "smooth" });
-  } else if (currentSection === "portrait") {
-    currentSection = "skills";
-    document.getElementById("skills").scrollIntoView({ behavior: "smooth" });
-    buttonArrowDown.style.display = "none";
-  }
-}
-
-function goTop() {
-  const buttonArrowUp = document.getElementById("buttonArrowUp");
-  const buttonArrowDown = document.getElementById("buttonArrowDown");
-
-  buttonArrowDown.style.display = "block";
-  buttonArrowDown.value = "navbar";
-
-  if (currentSection !== "navbar") {
-    currentSection = "navbar"
-    document.getElementById("navbar").scrollIntoView({ behavior: "smooth" });
-    buttonArrowUp.style.display = "none";
-  }
-}
